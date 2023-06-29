e720c57a8beaf3d6670ec90a49b7b45caa936adc
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-06-29 09:36:58 +0300

    repo alustettu

---
+++ src/main/java/com/mycompany/harjoitus3/Harjoitus3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.harjoitus3;
+
+/**
+ *
+ * @author pekka
+ */
+public class Harjoitus3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
